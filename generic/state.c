049579f8e8ac6e157e2f1302b8b3b3b4a222d922
Gergo Liptak <gergoliptak00@gmail.com>
2024-03-15 18:13:44 +0100

    State added

---
+++ generic/state.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "state.h"
+
+void PrintState(State state)
+{
+    printf("%c", state);
+}
---
+++ generic/state.h
@@ -0,0 +1,12 @@
+#ifndef STATE_H
+#define STATE_H
+
+#define FILLED 219
+#define EMPTY 'X'
+#define UNKNOWN ' '
+
+typedef char State;
+
+void PrintState(State state);
+
+#endif
