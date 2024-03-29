dcd6bafbda4fb3589c247970e45b43aa7f19e12c
Gergo Liptak <gergoliptak00@gmail.com>
2024-03-29 19:00:55 +0100

    Test cases added

--- main.c
+++ main.c
@@ -3,14 +3,18 @@
 #include "test_2.h"
 #include "test_3.h"
 #include "test_4.h"
+#include "test_5.h"
+#include "test_6.h"
 
 int main()
 {
     Test0();
-    // Test1(); // take too much time
+    Test1();
     Test2();
     Test3();
     Test4();
+    Test5();
+    // Test6(); // take too much time
 
     return 0;
 }
---
+++ tests/test_5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "board.h"
+#include "list.h"
+#include "solver.h"
+
+void Test5()
+{
+    List row_data0 = InitList(0);
+    AppendToList(&row_data0, 3);
+
+    List row_data1 = InitList(0);
+    AppendToList(&row_data1, 3);
+
+    List row_data2 = InitList(0);
+    AppendToList(&row_data2, 1);
+
+    List row_data3 = InitList(0);
+    AppendToList(&row_data3, 1);
+    AppendToList(&row_data3, 1);
+
+    List row_data4 = InitList(0);
+    AppendToList(&row_data4, 1);
+
+    List column_data0 = InitList(0);
+    AppendToList(&column_data0, 1);
+
+    List column_data1 = InitList(0);
+    AppendToList(&column_data1, 2);
+    AppendToList(&column_data1, 1);
+
+    List column_data2 = InitList(0);
+    AppendToList(&column_data2, 3);
+    AppendToList(&column_data2, 1);
+
+    List column_data3 = InitList(0);
+    AppendToList(&column_data3, 1);
+
+    List column_data4 = InitList(0);
+    AppendToList(&column_data4, 1);
+    DefinitionList definition_list_rows = InitDefinitionList(5);
+    DefinitionList definition_list_columns = InitDefinitionList(5);
+    definition_list_rows.data[0] = InitDefinition(5, row_data0);
+    definition_list_rows.data[1] = InitDefinition(5, row_data1);
+    definition_list_rows.data[2] = InitDefinition(5, row_data2);
+    definition_list_rows.data[3] = InitDefinition(5, row_data3);
+    definition_list_rows.data[4] = InitDefinition(5, row_data4);
+    definition_list_columns.data[0] = InitDefinition(5, column_data0);
+    definition_list_columns.data[1] = InitDefinition(5, column_data1);
+    definition_list_columns.data[2] = InitDefinition(5, column_data2);
+    definition_list_columns.data[3] = InitDefinition(5, column_data3);
+    definition_list_columns.data[4] = InitDefinition(5, column_data4);
+    Board board = InitBoard(definition_list_columns, definition_list_rows);
+    SolveBoard(&board);
+}
