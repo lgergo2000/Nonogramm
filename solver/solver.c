ef77450dd6b625ddfbda5daced2e03b6d2443068
Gergo Liptak <gergoliptak00@gmail.com>
2024-03-29 18:59:24 +0100

    Priority loop created

--- solver/solver.c
+++ solver/solver.c
@@ -2,30 +2,62 @@
 #include "solver.h"
 #include "fill_matrix_with_vector.h"
 #include "withdraw.h"
+// #include "definition.h"
+
+typedef struct
+{
+    int index;
+    int possible_vector_length;
+} VectorLengthDict;
+
 
 void SolveBoard(Board* board)
 {
-    int main_index = 0;
+    int iter_index = 0;
+    int main_index;
 #ifdef VERBOSE
     int cnt = 0;
 #endif
+
+    VectorLengthDict* vector_length_dict = (VectorLengthDict*)calloc(board->definitions.length, sizeof(VectorLengthDict));
+    for (int i = 0; i < board->definitions.length; i++)
+    {
+        vector_length_dict[i] = (VectorLengthDict) {i, board->definitions.data[i].possible_vectors.length};
+    }
+
+    for (int i = 0; i < board->definitions.length - 1; ++i)
+    {
+        for (int j = 0; j < board->definitions.length - i - 1; ++j)
+        {
+            if (vector_length_dict[j].possible_vector_length > vector_length_dict[j+1].possible_vector_length)
+            {
+                VectorLengthDict vector_length_dict_tmp = vector_length_dict[j];
+
+                vector_length_dict[j] = vector_length_dict[j+1];
+                vector_length_dict[j+1] = vector_length_dict_tmp;
+            }
+        }
+    }
+
     Vector tmp;
     if(board->number_of_columns > board->number_of_rows) tmp = InitVector(board->number_of_columns);
     else tmp = InitVector(board->number_of_rows);
     while (TRUE)
     {
         int set = -1;
+        main_index = vector_length_dict[iter_index].index;
         // iterates thought the possible vectors
         for (int definitions_sub_index = board->definitions.data[main_index].index + 1; definitions_sub_index < board->definitions.data[main_index].possible_vectors.length; definitions_sub_index++)
         {
             set = FillMatrixWithVectorIfPossible(&(board->matrix), &(board->definitions.data[main_index].possible_vectors.data[definitions_sub_index]), main_index, &tmp);
             if(set == 1)
             {
+
 #ifdef VERBOSE
                 int max;
                 if(board->number_of_columns > board->number_of_rows) max = board->number_of_columns;
                 else max = board->number_of_rows;
-                if(main_index > ((float)max*2)/3)
+                if(iter_index > ((float)max*2)/3)
                 {
                     printf("Possible solution found: %d\n", cnt++);
                     PrintMatrix(board->matrix);
@@ -38,8 +70,8 @@ void SolveBoard(Board* board)
         }
         if(set == 1)
         {
-            main_index++;
-            if(main_index == board->definitions.length)
+            iter_index++;
+            if(iter_index == board->definitions.length)
             {
                 printf("Result:\n");
                 PrintMatrix(board->matrix);
@@ -48,13 +80,18 @@ void SolveBoard(Board* board)
         }
         else if(set == -1)
         {
-            if(main_index == 0)
+            if(iter_index == 0)
             {
                 printf("No solution\n");
                 break;
             }
-            WithdrawDefinition(&(board->matrix), --main_index);
-            board->definitions.data[main_index+1].index = -1;
+            board->definitions.data[main_index].index = -1;
+            main_index = vector_length_dict[--iter_index].index;
+            int tmp_index = board->definitions.data[main_index].index;
+            board->definitions.data[main_index].index = -1;
+            WithdrawDefinition(board, &tmp);
+            board->definitions.data[main_index].index = tmp_index;
+
         }
     }
 }
